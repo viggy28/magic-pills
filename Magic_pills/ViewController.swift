89ccee01de546c2f6848d6b54ddcf2b9013dbcd9
Vignesh Ravichandran <vignesh.ravichandran@ticketmaster.com>
2017-01-06 22:51:18 -0800

    Initial Commit

---
+++ Magic_pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic_pills
+//
+//  Created by Vignesh Ravichandran on 1/6/17.
+//  Copyright © 2017 Vignesh Ravichandran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Magic_pillsTests/Magic_pillsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Magic_pillsTests.swift
+//  Magic_pillsTests
+//
+//  Created by Vignesh Ravichandran on 1/6/17.
+//  Copyright © 2017 Vignesh Ravichandran. All rights reserved.
+//
+
+import XCTest
+@testable import Magic_pills
+
+class Magic_pillsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
